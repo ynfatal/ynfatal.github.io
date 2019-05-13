be92690a4a76a11cd2ca904864dd7d9f08f5064c
ynFatal <63413763@qq.com>
2019-05-13 13:16:54 +0800

    Site updated: 2019-05-13 13:16:53

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -19,7 +19,6 @@ copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
           console.info('Action:', e.action);
           console.info('Text:', e.text);
           console.info('Trigger:', e.trigger);
-          $("button.btn-copy span").text("success");
           e.clearSelection();
       });
     }
