24692472248e0855e02de87f1ce9cb7e6192abcc
ynFatal <63413763@qq.com>
2019-05-13 16:26:21 +0800

    Site updated: 2019-05-13 16:26:19

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,32 +1,3 @@
-$(function() {
-  /*页面载入完成后，创建复制按钮*/
-  !function (e, t, a) { 
-    /* code */
-    var initCopyCode = function(){
-      var copyHtml = '';
-      copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-      //fa fa-globe可以去字体库替换自己想要的图标
-copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
-      copyHtml += '</button>';
-      $(".highlight .code pre").before(copyHtml);
-      var clipboard = new ClipboardJS('.btn-copy', {
-          target: function(trigger) {
-              return trigger.nextElementSibling;
-          }
-      });
-      // 复制成功触发的事件
-      clipboard.on('success', function(e) {
-          console.info('Action:', e.action);
-          console.info('Text:', e.text);
-          console.info('Trigger:', e.trigger);
-          e.clearSelection();
-          var btn = e.trigger;
-          $(btn).tooltip({text: '这是一个Tooltip！'});
-      });
-    }
-    initCopyCode();
-  }(window, document);
-});
 var ToolTip = {
     transitionInterval: null,
     transitionDelayTimeout: null,
@@ -404,3 +375,32 @@ $(function($){
         }
     }
 }(jQuery));
+$(function() {
+  /*页面载入完成后，创建复制按钮*/
+  !function (e, t, a) { 
+    /* code */
+    var initCopyCode = function(){
+      var copyHtml = '';
+      copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+      //fa fa-globe可以去字体库替换自己想要的图标
+copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
+      copyHtml += '</button>';
+      $(".highlight .code pre").before(copyHtml);
+      var clipboard = new ClipboardJS('.btn-copy', {
+          target: function(trigger) {
+              return trigger.nextElementSibling;
+          }
+      });
+      // 复制成功触发的事件
+      clipboard.on('success', function(e) {
+          console.info('Action:', e.action);
+          console.info('Text:', e.text);
+          console.info('Trigger:', e.trigger);
+          e.clearSelection();
+          var btn = e.trigger;
+          $(btn).tooltip({text: '这是一个Tooltip！'});
+      });
+    }
+    initCopyCode();
+  }(window, document);
+});
