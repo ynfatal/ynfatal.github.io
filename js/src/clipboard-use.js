24a769a9ac9e88bb31aa38c4e62ac4c5a3aa05a8
ynFatal <63413763@qq.com>
2019-05-13 14:30:28 +0800

    Site updated: 2019-05-13 14:30:27

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,16 +1,16 @@
-function setTooltip(btn, message) {
-  btn.tooltip('hide')
-    .attr('data-original-title', message)
-    .tooltip('show');
-}
-
-function hideTooltip(btn) {
-  setTimeout(function() {
-    btn.tooltip('hide');
-  }, 1000);
-}
-
 $(function() {
+  function setTooltip(btn, message) {
+    btn.tooltip('hide')
+      .attr('data-original-title', message)
+      .tooltip('show');
+  }
+
+  function hideTooltip(btn) {
+    setTimeout(function() {
+      btn.tooltip('hide');
+    }, 1000);
+  }
+
   /*页面载入完成后，创建复制按钮*/
   !function (e, t, a) { 
     /* code */
