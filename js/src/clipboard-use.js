cc17e55c3919d6dee54c9b1c146589a4c6fc3164
ynFatal <63413763@qq.com>
2019-05-13 13:50:46 +0800

    Site updated: 2019-05-13 13:50:45

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -23,12 +23,12 @@ $(function() {
     /* code */
     var initCopyCode = function(){
       var copyHtml = '';
-      copyHtml += '<button class="btn" data-clipboard-snippet="">';
+      copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
       //fa fa-globe可以去字体库替换自己想要的图标
 copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
       copyHtml += '</button>';
       $(".highlight .code pre").before(copyHtml);
-      var clipboard = new ClipboardJS('.btn', {
+      var clipboard = new ClipboardJS('.btn-copy', {
           target: function(trigger) {
               return trigger.nextElementSibling;
           }
